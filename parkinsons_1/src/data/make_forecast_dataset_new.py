12b0fb414a21e96fcc566bc9c3966b858c5108c2
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2023-05-06 14:20:24 -0700

    update to create forecast with visit difference

--- parkinsons_1/src/data/make_forecast_dataset_new.py
+++ parkinsons_1/src/data/make_forecast_dataset_new.py
@@ -1,43 +1,108 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
+import itertools
+
+updr = "updrs_1"
+patient_id = 55
 
 
+# iterate through each and give the visit_month difference and updrs difference
+def calculate_updrs_diff(forecast_df, patient_id, updr):
+    """
+    Calculate the difference in UPDRS score between visits for each row in the dataset.
 
-def preprocess_forecast_train_df(forecast_data, target):
-    
-    temp_df = forecast_data[['visit_id', 'patient_id', target, 'visit_month']].sort_values(by=['patient_id', 'visit_month']).reset_index(drop=True)
-    temp_pivot = temp_df.pivot(columns='visit_month', values=target, index='patient_id')
-    temp_pivot = temp_pivot.reset_index()
-    
-    cols = [f'{target}_{month}' for month in temp_pivot.columns[1:]]
-    temp_pivot.columns = ['patient_id'] + cols
-    
-    forecast_final = forecast_data[forecast_data['visit_month'] == 0]
-    forecast_final.head()
-    
-    final_df = forecast_final.merge(temp_pivot, on=['patient_id'], how='left')
-    
-    final_df = final_df.drop(columns=['patient_id', target])
-    if 'kfold' in final_df.columns:
-        final_df = final_df.drop(columns=['kfold'])
-        
-    
-    file_path = f'~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/forecast_train_{target}.csv'
+    Args:
+        df (pd.DataFrame): The dataset for only one patient_id and only the rows that have protein data.
 
-    print('Preprocessing complete for target: ', target)
-    print('Saving file to: ', file_path, '\n')
-    
-    final_df.to_csv(file_path, index=False)
+    Returns:
+        pd.DataFrame: The dataset with the updrs_diff column added.
+    """
+
+    forecast_df = forecast_df[forecast_df["patient_id"] == patient_id]
+    df = forecast_df[[updr, "visit_month", "patient_id"]]
+
+    # generate all possible pairs of visit months
+    pairs = list(itertools.product(df["visit_month"], df["visit_month"]))
+
+    # create a dataframe with all possible pairs of visit months
+    joined_df = pd.DataFrame(
+        {
+            "visit_month": [pair[0] for pair in pairs],
+            "visit_month_2": [pair[1] for pair in pairs],
+        }
+    )
+
+    # calculate the difference in months between the two visits
+    joined_df["visit_month_diff"] = (
+        joined_df["visit_month_2"] - joined_df["visit_month"]
+    )
+
+    # drop visit_month_dif == 0 rows
+    joined_df = joined_df[joined_df["visit_month_diff"] != 0]
+
+    # join this back to the original df
+    joined_df = pd.merge(joined_df, df, on=["visit_month"], how="inner")
+
+    # calculate the mean UPDRS score for each visit month
+    updrs = joined_df.groupby(["visit_month"])[updr].mean().reset_index()
+    updrs = updrs.rename(columns={updr: f"{updr}_visit2"})
+
+    # join this back to the original df
+    joined_df = joined_df.merge(
+        updrs, left_on=["visit_month_2"], right_on=["visit_month"], how="left"
+    )
+    joined_df = joined_df.drop(columns=["visit_month_y"]).rename(
+        columns={"visit_month_x": "visit_month"}
+    )
+
+    # calculate the difference in UPDRS score between the two visits
+    joined_df["updrs_diff"] = joined_df[f"{updr}_visit2"] - joined_df[updr]
+
+    # drop the unneeded columns
+    joined_df = joined_df.drop(columns=["visit_month_2", f"{updr}_visit2"])
+
+    final_df = joined_df.merge(
+        forecast_df, on=["patient_id", "visit_month"], how="left"
+    )
+
+    final_df = final_df.drop(columns=[f"{updr}_y"]).rename(columns={f"{updr}_x": updr})
+
+    return final_df
 
 
+if __name__ == "__main__":
+    train_clin_full_df = pd.read_csv(
+        "~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/raw/train_clinical_data.csv"
+    )
 
+    final_forecast_df = pd.DataFrame()
 
+    for updr in ["updrs_1", "updrs_2", "updrs_3", "updrs_4"]:
+        # get only the data for updr, visit_id, visit_month, patient_id
+        train_clin_df = train_clin_full_df[
+            [updr, "visit_id", "visit_month", "patient_id"]
+        ]
 
-if __name__ == '__main__':
-    
-    for target in ['updrs_1', 'updrs_2', 'updrs_3', 'updrs_4']:
-    
-        forecast_data = pd.read_csv(f'~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/train_{target}.csv')
-        preprocess_forecast_train_df(forecast_data, target)
-        
+        # get the months that have the protein data
+        forecast_data = pd.read_csv(
+            f"~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/train_{updr}.csv"
+        )
+
+        # join the train_clin with the forecast_data
+        forecast_df = pd.merge(
+            train_clin_df,
+            forecast_data.drop(columns=["visit_month", "patient_id", updr]),
+            on=["visit_id"],
+            how="inner",
+        )
+
+        for patient_id in forecast_df["patient_id"].unique():
+            temp_df = calculate_updrs_diff(forecast_df, patient_id, updr)
+            final_forecast_df = pd.concat([final_forecast_df, temp_df])
+
+    # save the final results
+    final_forecast_df.to_csv(
+        "~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/train_forecast_visitdiff_data.csv",
+        index=False,
+    )
