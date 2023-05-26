c89a1d1e360ceb6d7b3a3f2c6bc12892d369c85d
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2023-05-25 22:35:00 -0700

    create xgboost hyperopt file

---
+++ parkinsons_1/src/xgboost_train.py
@@ -0,0 +1,101 @@
+# xgboost_train.py
+
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from functools import partial
+from sklearn.metrics import mean_squared_error
+from sklearn import model_selection
+
+from hyperopt import hp, fmin, tpe, Trials
+from hyperopt.pyll.base import scope
+
+
+def optmize(params, param_names, X, y):
+    """
+    The main optimization function. This function will take all the arguments
+    from the search space and training features and labels. It will then initialize
+    the models by setting the chosen parameters and run cross-validation and return a
+    mean squared error score as a result.
+
+    Args:
+        params (list): list of params from gp_minimize
+        param_names (list): parameter names
+        X (dataframe): training data
+        y (series): target values
+
+    return: mean squared error after cross validation
+
+    """
+
+    # convert params to dictionary
+    params = dict(zip(param_names, params))
+
+    # initialize model with current parameters
+    model = xgb.XGBRegressor(**params)
+
+    # initialize stratified k-fold
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    # initialize mse list
+    mse_list = []
+
+    # loop over all folds
+    for idx in kf.split(X=X, y=y):
+        train_idx, val_idx = idx[0], idx[1]
+
+        # training and validation data
+        X_train = X.iloc[train_idx]
+        y_train = y.iloc[train_idx]
+
+        X_val = X.iloc[val_idx]
+        y_val = y.iloc[val_idx]
+
+        # fit model for current fold
+        model.fit(X_train, y_train)
+
+        # create predictions
+        preds = model.predict(X_val)
+
+        # calculate and append mse score
+        fold_mse = mean_squared_error(y_val, preds)
+        mse_list.append(fold_mse)
+
+    # return mean of mse scores
+    return np.mean(mse_list)
+
+
+if __name__ == "__main__":
+    # read the training data
+    df = pd.read_csv(
+        "~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/train_updrs_1.csv"
+    )
+
+    # define the parameter space
+    param_space = {
+        "max_depth": scope.int(hp.quniform("max_depth", 3, 20, 1)),
+        "min_child_weight": scope.int(hp.quniform("min_child_weight", 1, 7, 1)),
+        "gamma": hp.uniform("gamma", 0.05, 1),
+        "eta": hp.uniform("eta", 0.05, 1),
+        "subsample": hp.uniform("subsample", 0.6, 1),
+        "colsample_bytree": hp.uniform("colsample_bytree", 0.6, 1),
+        "lambda": hp.uniform("lambda", 0.01, 0.1),
+        "alpha": hp.uniform("alpha", 0.01, 1.0),
+    }
+
+    # partial function
+    optimization_function = partial(optmize, x=X, y=y)
+
+    # initialize trials to keep logging information
+    trials = Trials()
+
+    # run hyperopt
+    hopt = fmin(
+        fn=optimization_function,
+        space=param_space,
+        algo=tpe.suggest,
+        max_evals=15,
+        trials=trials,
+    )
+
+    print(hopt)
