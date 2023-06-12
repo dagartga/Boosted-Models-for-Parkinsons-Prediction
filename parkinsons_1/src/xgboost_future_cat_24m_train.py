8d60ac4a224ec19ca650f2c7f05aa11aacab29ce
Dagart Allison <65997824+dagartga@users.noreply.github.com>
2023-06-11 20:07:11 -0700

    hyperparam opts on prot diff data

---
+++ parkinsons_1/src/xgboost_future_cat_24m_train.py
@@ -0,0 +1,152 @@
+# xgboost_train.py
+
+import numpy as np
+import pandas as pd
+import pickle
+from functools import partial
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn import model_selection
+from typing import Any, Dict, Union
+
+from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
+from hyperopt.pyll.base import scope
+
+
+def hyperparameter_tuning(
+    space: Dict[str, Union[float, int]],
+    X_train: pd.DataFrame,
+    y_train: pd.Series,
+    X_test: pd.DataFrame,
+    y_test: pd.Series,
+    early_stopping_rounds: int = 50,
+    metric: callable = roc_auc_score,
+) -> Dict[str, Any]:
+    init_vals = ["max_depth", "reg_alpha"]
+    space = {k: (int(val) if k in init_vals else val) for k, val in space.items()}
+    space["early_stopping_rounds"] = early_stopping_rounds
+    model = XGBClassifier(**space)
+    evaluation = [(X_train, y_train), (X_test, y_test)]
+    model.fit(
+        X_train,
+        y_train,
+        early_stopping_rounds=20,
+        eval_metric="auc",
+        eval_set=evaluation,
+        verbose=False,
+    )
+
+    pred = model.predict(X_test)
+    score = metric(y_test, pred)
+    return {"loss": -score, "status": STATUS_OK, "model": model}
+
+
+def create_kfolds(df, updrs):
+    # create a new column for kfold and fill it with -1
+    df["kfold"] = -1
+
+    # randomize the rows of the data
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # calculate the number of bins using Sturge's rule
+    # I am using the max here to ensure that the number of bins is at least 5
+    # and at most 12
+    num_bins = int(np.floor(1 + np.log2(len(df))))
+
+    # bin targets
+    df.loc[:, "bins"] = pd.cut(df[f"{updrs}_max"], bins=num_bins, labels=False)
+
+    # initiate the kfold class from model_selection module
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    # fill the new kfold column
+    # note that instead of targets we are using bins!
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.bins.values)):
+        df.loc[val_idx, "kfold"] = fold
+
+    # drop the bins column
+    df = df.drop("bins", axis=1)
+
+    # return dataframe with folds
+    return df
+
+
+if __name__ == "__main__":
+    # read the training data
+    df = pd.read_csv(
+        "~/parkinsons_proj_1/parkinsons_project/parkinsons_1/data/processed/train_24month_protein_data.csv"
+    )
+
+    # get only the updrs of interest
+    # the updrs_1_max is the target, which means the highest categorical value
+    # 0 is mild parkinsons, and 1 is moderate to severe parkinsons as the max
+    updrs1_df = df.drop(columns=["updrs_2_max", "updrs_3_max"])
+    updrs2_df = df.drop(columns=["updrs_1_max", "updrs_3_max"])
+    updrs3_df = df.drop(columns=["updrs_1_max", "updrs_2_max"])
+
+    updrs_results = dict()
+
+    for updrs, df in zip(
+        ["updrs_1", "updrs_2", "updrs_3"], [updrs1_df, updrs2_df, updrs3_df]
+    ):
+        # preprocess the df
+        df = create_kfolds(df, updrs)
+
+        train = df[df["kfold"] != 4].reset_index(drop=True)
+        test = df[df["kfold"] == 4].reset_index(drop=True)
+
+        X_train = train.drop(
+            columns=[
+                "kfold",
+                "patient_id",
+                f"{updrs}_max",
+            ]
+        )
+        y_train = train[f"{updrs}_max"]
+        X_test = test.drop(
+            columns=[
+                "kfold",
+                "patient_id",
+                f"{updrs}_max",
+            ]
+        )
+        y_test = test[f"{updrs}_max"]
+
+        label_encoder = LabelEncoder()
+        y_train = label_encoder.fit_transform(y_train)
+        y_test = label_encoder.fit_transform(y_test)
+
+        options = {
+            "max_depth": hp.quniform("max_depth", 1, 8, 1),
+            "min_child_weight": hp.loguniform("min_child_weight", -2, 3),
+            "subsample": hp.uniform("subsample", 0.5, 1),
+            "colsample_bytree": hp.uniform("colsample_bytree", 0.5, 1),
+            "reg_alpha": hp.uniform("reg_alpha", 0, 10),
+            "reg_lambda": hp.uniform("reg_lambda", 1, 10),
+            "gamma": hp.loguniform("gamma", -10, 10),
+            "learning_rate": hp.loguniform("learning_rate", -7, 0),
+            "random_state": 42,
+        }
+
+        trials = Trials()
+        best = fmin(
+            fn=lambda space: hyperparameter_tuning(
+                space, X_train, y_train, X_test, y_test
+            ),
+            space=options,
+            algo=tpe.suggest,
+            max_evals=100,
+            trials=trials,
+        )
+
+        updrs_results[updrs] = best
+
+    # save the results
+    updrs_results_df = pd.DataFrame(updrs_results)
+
+    # save as a csv file
+    updrs_results_df.to_csv(
+        "../data/processed/xgboost_future_cat_24m_hyperparam_results.csv", index=True
+    )
