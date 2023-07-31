1cb56fbfc1a5faf355f232b00422682281f21f3e
Dagart Allison <dagartga@gmail.com>
2023-07-30 18:26:45 -0700

    remove uneeded files

--- parkinsons_1/src/model_dispatcher.py
+++
@@ -1,9 +0,0 @@
-# model_dispatcher.py
-
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from xgboost import XGBRegressor
-
-models = {"rf_reg": RandomForestRegressor(random_state = 42),
-          "xgboost": XGBRegressor(random_state = 42),
-          "rf_class": RandomForestClassifier(random_state = 42),
-         }
